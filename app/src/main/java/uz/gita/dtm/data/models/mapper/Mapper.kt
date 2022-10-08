d8bfce5380b061245702853e75fcb3c4301e64d6
Shahrizod <qosimovshaxrizod7376@gmail.com>
2022-10-08 17:19:13 +0500

    repositories ready

--- app/src/main/java/uz/gita/dtm/data/models/mapper/Mapper.kt
+++ app/src/main/java/uz/gita/dtm/data/models/mapper/Mapper.kt
@@ -6,6 +6,7 @@ import uz.gita.dtm.data.models.persondata.ApplicantAddress
 import uz.gita.dtm.data.models.persondata.Application
 import uz.gita.dtm.data.models.persondata.Education
 import uz.gita.dtm.data.models.persondata.Passport
+import uz.gita.dtm.data.models.service.Service
 
 object Mapper {
 
@@ -59,5 +60,12 @@ object Mapper {
         image = this["image"].toString(),
         title = this["title"].toString(),
         date = this["date"].toString().toLong()
+    )
+
+    fun DocumentSnapshot.toService() = Service(
+        id = this["id"].toString(),
+        title = this["title"].toString(),
+        image = this["image"].toString(),
+        desc = this["desc"].toString()
     )
 }
--- app/src/main/java/uz/gita/dtm/domain/repository/service/impl/ServiceRepositoryImpl.kt
+++ app/src/main/java/uz/gita/dtm/domain/repository/service/impl/ServiceRepositoryImpl.kt
@@ -1,18 +1,43 @@
 package uz.gita.dtm.domain.repository.service.impl
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import uz.gita.dtm.data.models.mapper.Mapper.toApplication
+import uz.gita.dtm.data.models.mapper.Mapper.toService
 import uz.gita.dtm.data.models.persondata.Application
 import uz.gita.dtm.data.models.service.Service
 import uz.gita.dtm.data.utils.ResultData
 import uz.gita.dtm.domain.repository.service.ServiceRepository
 import javax.inject.Inject
 
-class ServiceRepositoryImpl @Inject constructor():ServiceRepository {
-    override fun getServiceList(): Flow<List<Service>> {
-        TODO("Not yet implemented")
+class ServiceRepositoryImpl @Inject constructor() : ServiceRepository {
+
+    private val db = Firebase.firestore
+
+    override fun getServiceList(): Flow<List<Service>> = callbackFlow {
+
+        val serviceList = db.collection("services").addSnapshotListener { value, error ->
+            val data = value?.map {
+                it.toService()
+            }
+            trySend(data ?: emptyList())
+        }
+        awaitClose { serviceList.remove() }
     }
 
-    override fun getApplicationById(applicantId: String): Flow<Application> {
-        TODO("Not yet implemented")
+    override fun getApplicationById(applicantId: String): Flow<Application> = callbackFlow {
+
+
+        val application = db.collection("applications").addSnapshotListener { value, error ->
+            val data = value?.map {
+                it.toApplication()
+            }?.filter { it -> it.id == applicantId }?.toList()?.get(0)
+
+            trySend(data!!)
+        }
+        awaitClose { application.remove() }
     }
 }
