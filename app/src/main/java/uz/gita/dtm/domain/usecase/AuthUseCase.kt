07f664cfef8d51367653843c47ac1ba55bc8cc2b
Azimjon <makhmudovazimjon53@gmail.com>
2022-10-08 14:45:43 +0500

    add hech narsa

---
+++ app/src/main/java/uz/gita/dtm/domain/usecase/AuthUseCase.kt
@@ -0,0 +1,6 @@
+package uz.gita.dtm.domain.usecase
+
+
+interface AuthUseCase {
+
+}
--- app/src/main/java/uz/gita/dtm/presentation/ui/viewmodel/LoginScreenViewModel.kt
+++ app/src/main/java/uz/gita/dtm/presentation/ui/viewmodel/LoginScreenViewModel.kt
@@ -1,5 +1,15 @@
 package uz.gita.dtm.presentation.ui.viewmodel
 
+import android.app.Activity
+import androidx.lifecycle.LiveData
+
 interface LoginScreenViewModel {
+    val messageLiveData: LiveData<Int>
+    val openRegistrationScreen: LiveData<Unit>
+    val messageForPhoneNumber: LiveData<Int>
+    val messageForPassword: LiveData<Int>
+
+    fun btnLogin(activity: Activity, phoneNumber: String, password: String)
+    fun openRegistrationScreen()
 
 }
