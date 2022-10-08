d356279fe98f47ca67f1dbb3292115c8b37f4834
Gilfoyle-dev <bek.droid.dev@gmail.com>
2022-10-08 13:44:42 +0500

    Test-update

--- app/src/main/java/uz/gita/dtm/data/shp/MySharedPreference.kt
+++ app/src/main/java/uz/gita/dtm/data/shp/MySharedPreference.kt
@@ -1,4 +1,5 @@
 package uz.gita.dtm.data.shp
 
 class MySharedPreference {
+
 }
--- app/src/main/java/uz/gita/dtm/domain/repository/news/impl/NewsRepositoryImpl.kt
+++ app/src/main/java/uz/gita/dtm/domain/repository/news/impl/NewsRepositoryImpl.kt
@@ -1,4 +1,5 @@
 package uz.gita.dtm.domain.repository.news.impl
 
 class NewsRepositoryImpl {
+//    private val firestore = FirebaseFirestore.getInstance()
 }
