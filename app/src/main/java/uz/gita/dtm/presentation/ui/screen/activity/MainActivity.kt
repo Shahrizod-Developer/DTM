4d568de59e631fdc6704c711a5ff1acbfb11fb78
Shoxruh Zayniddinov <zayniddinovshoxruh6@gmail.com>
2022-10-14 22:23:01 +0500

    -added broadcast

--- app/src/main/java/uz/gita/dtm/presentation/ui/screen/activity/MainActivity.kt
+++ app/src/main/java/uz/gita/dtm/presentation/ui/screen/activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package uz.gita.dtm.presentation.ui.screen.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -10,22 +12,29 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import uz.gita.dtm.R
 import uz.gita.dtm.presentation.navigation.Handler
+import uz.gita.dtm.utils.InternetBroadCast
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var handler: Handler
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val broadCast = InternetBroadCast(this)
+        registerReceiver(
+            broadCast,
+            IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+        )
+        broadCast.check { finish() }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
 
         handler.navigationStack
             .onEach {
                 it.invoke(navHostFragment.findNavController())
             }.launchIn(lifecycleScope)
     }
+
 }
---
+++ app/src/main/java/uz/gita/dtm/utils/InternetBroadCast.kt
@@ -0,0 +1,69 @@
+package uz.gita.dtm.utils
+
+
+import android.app.Dialog
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.widget.AppCompatButton
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import uz.gita.dtm.R
+
+
+class InternetBroadCast(context: Context) : BroadcastReceiver() {
+    private val db = Firebase.firestore
+    lateinit var dialog: Dialog
+    lateinit var view: View
+    private var block: (() -> Unit)? = null
+
+    init {
+        view = LayoutInflater.from(context).inflate(R.layout.overlay_no_internet, null)
+        dialog = Dialog(context)
+        dialog.setContentView(
+            view
+        )
+        dialog.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.MATCH_PARENT
+        )
+        dialog.setCancelable(false)
+        view.findViewById<AppCompatButton>(R.id.confirm).setOnClickListener {
+            block?.invoke()
+        }
+    }
+
+    fun check(chain: () -> Unit) {
+        block = chain
+    }
+
+    override fun onReceive(context: Context, intent: Intent?) {
+        val connMgr = context
+            .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val wifi = connMgr
+            .getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+        val mobile = connMgr
+            .getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+        if (wifi!!.isAvailable || mobile!!.isAvailable) {
+            db.clearPersistence()
+            val docRef = db.collection("pin").document("connection")
+            docRef.get().addOnSuccessListener {
+                if (it.metadata.isFromCache) {
+                    Log.d("TTT", "ishla")
+                    dialog.show()
+                } else {
+                    dialog.hide()
+                }
+            }.addOnFailureListener {
+                dialog.show()
+            }
+                .addOnCanceledListener { dialog.show() }
+        }
+    }
+
+}
