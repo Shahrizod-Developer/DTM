2ead5885e7186417798858c0bd35e9270e712f97
Shoxruh Zayniddinov <zayniddinovshoxruh6@gmail.com>
2022-10-14 22:23:01 +0500

    -added broadcast

--- app/src/main/java/uz/gita/dtm/app/App.kt
+++ app/src/main/java/uz/gita/dtm/app/App.kt
@@ -1,13 +1,19 @@
 package uz.gita.dtm.app
 
 import android.app.Application
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import dagger.hilt.android.HiltAndroidApp
 import uz.gita.dtm.data.shp.MySharedPreference
+import uz.gita.dtm.utils.InternetBroadCast
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         MySharedPreference.init(this)
+
+
     }
 }
